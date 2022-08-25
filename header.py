344506494b3695b42375a02967b315860db0cb9d
pier1580 <112120080+pier1580@users.noreply.github.com>
2022-08-25 12:27:16 -0700

    Add files via upload

---
+++ header.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 12:23:53 2022
+
+@author: Ignotus
+"""
+
+# ###############################################################
+# #
+# Chris Pierson #
+# ECE 351 #
+# Lab 0 #
+# Due 9/1/22 #
+# Any other necessary information needed to navigate the file #
+# #
+# ###############################################################
+
